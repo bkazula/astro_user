def705929a398b220380d47a3d5150e16097cdd5
Bartosz Kazuła <bartosz@kazula.eu>
2023-04-07 11:53:00 +0200

    Added config for prettier and eslint

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -5,7 +5,21 @@ return {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-      ensure_installed = { "lua_ls", "rust_analyzer", "cssls", "eslint", "gopls", "html", "jsonls", "tsserver", "marksman", "pyright", "rome", "sqls", "svelte" },
+      ensure_installed = {
+        "lua_ls",
+        "rust_analyzer",
+        "cssls",
+        "eslint",
+        "gopls",
+        "html",
+        "jsonls",
+        "tsserver",
+        "marksman",
+        "pyright",
+        "rome",
+        "sqls",
+        "svelte",
+      },
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
@@ -13,8 +27,36 @@ return {
     "jay-babu/mason-null-ls.nvim",
     -- overrides `require("mason-null-ls").setup(...)`
     opts = {
-      ensure_installed = { "prettier", "stylua", "rome", "black", "rustfmt" },
+      ensure_installed = { "prettier", "stylua", "rome", "black", "rustfmt", "eslint_d" },
     },
+    config = function(plugin, opts)
+      local mason_null_ls = require "mason-null-ls"
+      local null_ls = require "null-ls"
+
+      mason_null_ls.setup(opts) -- run setup
+      mason_null_ls.setup_handlers { -- setup custom handlers
+        prettier = function()
+          null_ls.register(null_ls.builtins.formatting.prettier.with {
+            condition = function(utils)
+              return utils.root_has_file "package.json"
+                or utils.root_has_file ".prettierrc"
+                or utils.root_has_file ".prettierrc.json"
+                or utils.root_has_file ".prettierrc.js"
+            end,
+          })
+        end,
+        -- For eslint_d:
+        eslint_d = function()
+          null_ls.register(null_ls.builtins.diagnostics.eslint_d.with {
+            condition = function(utils)
+              return utils.root_has_file "package.json"
+                or utils.root_has_file ".eslintrc.json"
+                or utils.root_has_file ".eslintrc.js"
+            end,
+          })
+        end,
+      }
+    end,
   },
   {
     "jay-babu/mason-nvim-dap.nvim",
