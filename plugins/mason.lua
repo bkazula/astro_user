c3223a7fa34433d645d4ff4e1b6d46944afcaf61
Bartosz Kazuła <bartosz@kazula.eu>
2023-04-11 14:18:57 +0200

    Sorted out formatting and moved config to null-ls instead of keeping it in mason

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -16,47 +16,10 @@ return {
         "tsserver",
         "marksman",
         "pyright",
-        "rome",
-        "sqls",
+        "sqlls",
         "svelte",
       },
     },
-  },
-  -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
-  {
-    "jay-babu/mason-null-ls.nvim",
-    -- overrides `require("mason-null-ls").setup(...)`
-    opts = {
-      ensure_installed = { "prettier", "stylua", "rome", "black", "rustfmt", "eslint_d" },
-    },
-    config = function(plugin, opts)
-      local mason_null_ls = require "mason-null-ls"
-      local null_ls = require "null-ls"
-
-      mason_null_ls.setup(opts) -- run setup
-      mason_null_ls.setup_handlers { -- setup custom handlers
-        prettier = function()
-          null_ls.register(null_ls.builtins.formatting.prettier.with {
-            condition = function(utils)
-              return utils.root_has_file "package.json"
-                or utils.root_has_file ".prettierrc"
-                or utils.root_has_file ".prettierrc.json"
-                or utils.root_has_file ".prettierrc.js"
-            end,
-          })
-        end,
-        -- For eslint_d:
-        eslint_d = function()
-          null_ls.register(null_ls.builtins.diagnostics.eslint_d.with {
-            condition = function(utils)
-              return utils.root_has_file "package.json"
-                or utils.root_has_file ".eslintrc.json"
-                or utils.root_has_file ".eslintrc.js"
-            end,
-          })
-        end,
-      }
-    end,
   },
   {
     "jay-babu/mason-nvim-dap.nvim",
--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -1,8 +1,9 @@
 return {
   "jose-elias-alvarez/null-ls.nvim",
   opts = function(_, config)
+    config.ensure_installed = { "stylua", "black", "rustfmt", "eslint_d" }
     -- config variable is the default configuration table for the setup function call
-    -- local null_ls = require "null-ls"
+    local null_ls = require "null-ls"
 
     -- Check supported formatters and linters
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -11,6 +12,13 @@ return {
       -- Set a formatter
       -- null_ls.builtins.formatting.stylua,
       -- null_ls.builtins.formatting.prettier,
+      null_ls.builtins.diagnostics.eslint_d.with {
+        condition = function(utils)
+          return utils.root_has_file "package.json"
+              or utils.root_has_file ".eslintrc.json"
+              or utils.root_has_file ".eslintrc.js"
+        end,
+      },
     }
     return config -- return final config table
   end,
